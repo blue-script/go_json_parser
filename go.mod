f0a2695c07c0177f591974e846e997ed6e38b503
blue-script <alexm92@inbox.ru>
2026-02-10 22:36:41 +0500

    feat: add iterate func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blue-script/go_json_parser
+
+go 1.25.4
--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"iter"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type JSONParser struct {
-	input string
+	input []byte
 }
 
 func (j *JSONParser) Parse() (any, error) {
@@ -16,13 +19,95 @@ func (j *JSONParser) Parse() (any, error) {
 	return map[string]any{"foo": "hello"}, nil
 }
 
-func NewJSONParser(input string) *JSONParser {
+func NewJSONParser(input []byte) *JSONParser {
 	return &JSONParser{
 		input: input,
 	}
 }
 
-func iterate() {}
+func Iterate(root any, path ...string) (iter.Seq2[any, any], error) {
+	if root == nil {
+		return nil, errors.New("No json data")
+	}
+
+	if len(path) == 0 {
+		switch v := root.(type) {
+		case map[string]any:
+			{
+				return func(yield func(any, any) bool) {
+					for key, value := range v {
+						if !yield(key, value) {
+							return
+						}
+					}
+				}, nil
+			}
+		case []any:
+			{
+				return func(yield func(any, any) bool) {
+					for i, value := range v {
+						if !yield(i, value) {
+							return
+						}
+					}
+				}, nil
+			}
+		default:
+			return nil, fmt.Errorf("Cannot iterate type %T\n", v)
+		}
+	}
+
+	cur := root
+	for i := range len(path) {
+		switch v := cur.(type) {
+		case []any:
+			{
+				num, err := strconv.Atoi(path[i])
+				if err != nil {
+					return nil, fmt.Errorf("invalid array index %q", path[i])
+				}
+				cur = v[num]
+			}
+		case map[string]any:
+			{
+				m, ok := v[path[i]]
+				if !ok {
+					return nil, fmt.Errorf("There isn't this key %s in map", path[i])
+				}
+				cur = m
+			}
+
+		default:
+			err := fmt.Sprintf("Cannot iterate type %T\n", cur)
+			return nil, errors.New(err)
+		}
+	}
+
+	switch v := cur.(type) {
+	case map[string]any:
+		{
+			return func(yield func(any, any) bool) {
+				for key, value := range v {
+					if !yield(key, value) {
+						return
+					}
+				}
+			}, nil
+		}
+	case []any:
+		{
+			return func(yield func(any, any) bool) {
+				for i, value := range v {
+					if !yield(i, value) {
+						return
+					}
+				}
+			}, nil
+		}
+	default:
+		return nil, fmt.Errorf("Cannot iterate type %T\n", v)
+	}
+}
 
 func main() {
 	// test()
@@ -31,25 +116,40 @@ func main() {
 	var err error
 
 	scanner := bufio.NewScanner(os.Stdin)
-
+CommandLoop:
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.SplitN(line, " ", 2)
-		// cmd := strings.ToLower(parts[0])
-		// arg := ""
-		if len(parts) < 2 {
+		cmd := strings.ToLower(parts[0])
+		arg := ""
+
+		if cmd != "exit" && len(parts) < 2 {
 			fmt.Println("Incorrect command")
 			continue
 		}
-		switch strings.ToLower(parts[0]) {
+
+		switch cmd {
 		case "parse":
-			parseJsonData, err = NewJSONParser(parts[1]).Parse()
+			parseJsonData, err = NewJSONParser([]byte(arg)).Parse()
 			fmt.Println(parseJsonData)
 			if err != nil {
 				fmt.Println("Error parsing JSON")
 				return
 			}
 		case "iterate":
+			Iterate(
+				map[string]any{
+					"foo": map[string]any{
+						"bar": map[string]any{
+							"baz": 42,
+							"qux": "hello",
+						},
+					},
+				},
+				"foo", "bar",
+			)
+		case "exit":
+			break CommandLoop
 		default:
 			fmt.Println("Incorrect command")
 		}
@@ -58,11 +158,13 @@ func main() {
 
 // func test() {
 // 	input := `{"foo": {"bar": {"baz": 42, "qux": "hello"}}}`
+// 	input := `[{"foo":"1"},{"foo":"2"},{"foo":"3"}]`
 // 	result, err := NewJSONParser(input).Parse()
 // 	if err != nil {
 // 		log.Fatalln("Error: invalid JSON")
 // 	}
 // 	path := []string{"foo", "bar"}
+// 	path := []string{"1"}
 // 	iter, err := PathIterator(result, path...)
 // 	if err != nil {
 // 		log.Fatalln("Error:", err)
